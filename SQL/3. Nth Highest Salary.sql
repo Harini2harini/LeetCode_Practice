6454f02823120c9e4b0af66f2641170e34ebb3cb
Harini <harini2510m@gmail.com>
2026-03-08 19:40:41 +0530

    Create 3. Nth Highest Salary.sql

---
+++ SQL/3. Nth Highest Salary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      SELECT salary
+      FROM Employee e1
+      WHERE N-1 = (
+          SELECT COUNT(DISTINCT salary)
+          FROM Employee e2
+          WHERE e2.salary > e1.salary
+      )
+      LIMIT 1
+
+  );
+END
